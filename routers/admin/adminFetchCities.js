0ed8c653d1f72cb7ca5293efee150e79c2a472c8
imloydurson <jlbillano29@gmail.com>
2023-11-28 20:10:55 +0800

    added most availed dishes and cities

---
+++ routers/admin/adminFetchCities.js
@@ -0,0 +1,24 @@
+// cityRouter.js
+
+const express = require('express');
+const router = express.Router();
+const { Sequelize } = require('sequelize');
+const Client = require("../../models/client");
+
+// Define the API endpoint to get city counts
+router.get('/cities', async (req, res) => {
+  try {
+    // Use Sequelize's `count` function to get counts for each city
+    const cityCounts = await Client.findAll({
+      attributes: ['client_address', [Sequelize.fn('COUNT', 'client_address'), 'count']],
+      group: ['client_address'],
+    });
+
+    res.json(cityCounts);
+  } catch (error) {
+    console.error('Error fetching city counts:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+module.exports = router;
