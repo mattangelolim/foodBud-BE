715252210434b5a96d833d8019bd22a419adac97
mattangeloblim <matt.lim@breddas.com.ph>
2024-01-11 18:11:11 +0800

    commit

--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -38,4 +38,21 @@ router.post("/user/login", async (req, res) => {
   }
 });
 
+router.get("/get/profile", async (req,res) =>{
+  try {
+    const email = req.query.email
+
+    const ClientProfile = await Client.findOne({
+      where:{
+        client_email:email
+      }
+    })
+
+    res.status(200).json({ClientProfile})
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({message: "Internal Server Error"})
+  }
+})
+
 module.exports = router;
